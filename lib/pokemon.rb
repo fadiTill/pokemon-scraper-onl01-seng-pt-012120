7e4e160e43de593e707dac0300fa2ffdf549b89d
fadimata tillman <fadi.tillman@gmail.com>
2020-04-13 22:09:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -28,7 +28,7 @@ class Pokemon
 #   end
 attr_accessor :id, :name, :type, :db
 
-   def initialize(id:,name:, :type:, db:)
+   def initialize(id:,name:, type:, db:)
      @id = id
      @name = name
      @type = type
